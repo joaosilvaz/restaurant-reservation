913a685db563dfa2e8972fdb39287c254748d8e2
Fernando1211 <aguiarfernando613@gmail.com>
2025-03-14 16:19:20 -0300

    [Fernando]- derletando a category

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -2,8 +2,6 @@ package com.example.demo.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale.Category;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
