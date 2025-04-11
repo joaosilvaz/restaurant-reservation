10c4e526694f3474e5554d7beea14a1427fc41aa
Fernando1211 <aguiarfernando613@gmail.com>
2025-04-10 23:17:02 -0300

    [Fernando]- finalizando o back

--- src/main/java/com/example/demo/controller/BookingController.java
+++ src/main/java/com/example/demo/controller/BookingController.java
@@ -2,7 +2,13 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Booking;
 import com.example.demo.repository.BookingRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,26 +21,69 @@ public class BookingController {
     private BookingRepository repository;
 
     @GetMapping
+    @Operation(
+            summary = "Listar todas as reservas",
+            description = "Listando as reservas criadas pelo usuário",
+            responses = {
+                    @ApiResponse(responseCode = "200")
+            }
+    )
     public List<Booking> index() {
         return repository.findAll();
     }
 
     @PostMapping
+    @Operation(
+            summary = "Cadastrar Reserva",
+            description = "Insere uma reserva",
+            responses = {
+                    @ApiResponse(responseCode = "201"),
+                    @ApiResponse(responseCode = "400")
+            }
+    )
+    @ResponseStatus(
+        code = HttpStatus.CREATED
+    )
     public Booking create(@RequestBody Booking booking) {
         return repository.save(booking);
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Listando as reservas por usuário",
+            description = "Demonstra as reservas que apenas o usuário realizou, ou seja, pelo (id)",
+            responses = {
+                    @ApiResponse(responseCode = "200")
+
+            }
+    )
     public Booking show(@PathVariable Long id) {
         return repository.findById(id).orElseThrow();
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Deletando a reserva do usuário",
+            description = "Deletando a reserva apenas que o usuário criou, ou seja ele deleta por ID",
+            responses = {
+                    @ApiResponse(responseCode = "200")
+
+            }
+    )
+
     public void delete(@PathVariable Long id) {
         repository.deleteById(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Alterando a reserva por usuário(ID)",
+            description = "Alterando a reserva apenas que o usuário criou, ou seja ela é alterada por ID",
+            responses = {
+                    @ApiResponse(responseCode = "200")
+
+            }
+    )
     public Booking update(@PathVariable Long id, @RequestBody Booking updatedBooking) {
         return repository.findById(id)
                 .map(b -> {
--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -2,7 +2,14 @@ package com.example.demo.controller;
 
 import com.example.demo.model.Usuario;
 import com.example.demo.repository.UsuarioRepository;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,26 +22,73 @@ public class UsuarioController {
     private UsuarioRepository repository;
 
     @GetMapping
+    @Cacheable ("usuarios")
+    @Operation(
+            summary = "Listar todos os usuários",
+            description = "Lista todos os usuários cadastrados",
+            responses = {
+                    @ApiResponse(responseCode = "200")
+
+            }
+    )
+    
     public List<Usuario> index() {
         return repository.findAll();
     }
 
     @PostMapping
+    @CacheEvict (value = "usuarios", allEntries = true)
+    @Operation(
+            summary = "Cadastrar usuário",
+            description = "Realizando o Cadastro de um usuário",
+            responses = {
+                    @ApiResponse(responseCode = "201"),
+                    @ApiResponse(responseCode = "400")
+            }
+    )
+     @ResponseStatus(
+        code = HttpStatus.CREATED
+    )
+
     public Usuario create(@RequestBody Usuario usuario) {
         return repository.save(usuario);
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Listar os usuários fornecidos por ID",
+            description = "Pegando as informações criada pelo usuário, ou seja ele pega por ID",
+            responses = {
+                    @ApiResponse(responseCode = "200")
+
+            }
+    )
     public Usuario show(@PathVariable Long id) {
         return repository.findById(id).orElseThrow();
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Deletar os usuários fornecidos por ID",
+            description = "Inserindo o ID do usuário é possível deleta-lo, vale ressaltar que deleta apenas o ID fornecido",
+            responses = {
+                    @ApiResponse(responseCode = "200")
+
+            }
+    )
     public void delete(@PathVariable Long id) {
         repository.deleteById(id);
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Alterar os usuários fornecidos por ID",
+            description = "Com o ID é possível alterar as infromações, utilizado bastante com após um erro de digitação ou uma alterção de senha ou e-mail",
+            responses = {
+                    @ApiResponse(responseCode = "200")
+
+            }
+    )
     public Usuario update(@PathVariable Long id, @RequestBody Usuario updatedUsuario) {
         return repository.findById(id)
                 .map(u -> {
